6fc3810a073a8379761dc76fdc11026488e40a1a
Lee Painter <lee@ludup.com>
2017-04-24 23:11:16 +0100

    Updated to execute app 10 times if exit code is non zero but quit on error or if shift key is held down.

--- ModalMe.cpp
+++ ModalMe.cpp
@@ -19,52 +19,61 @@ LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
 	try {
-  wchar_t* desktop   = L"NewDesktop";
+		wchar_t* desktop = L"NewDesktop";
 
-  HDESK hThreadDT = GetThreadDesktop(GetCurrentThreadId());
-  HDESK hInputDT  = OpenInputDesktop(0, FALSE, DESKTOP_SWITCHDESKTOP);
-  HDESK hNewDT    = CreateDesktop(desktop, NULL, NULL, 0, GENERIC_ALL, NULL);
+		HDESK hThreadDT = GetThreadDesktop(GetCurrentThreadId());
+		HDESK hInputDT  = OpenInputDesktop(0, FALSE, DESKTOP_SWITCHDESKTOP);
+		HDESK hNewDT    = CreateDesktop(desktop, NULL, NULL, 0, GENERIC_ALL, NULL);
 
-  SetThreadDesktop(hNewDT);
-  SwitchDesktop(hNewDT);
+		SetThreadDesktop(hNewDT);
+		SwitchDesktop(hNewDT);
 
-   PROCESS_INFORMATION pi;
-    STARTUPINFO si;
+		for (int i = 0; i < 10; i++) {
+			PROCESS_INFORMATION pi;
+			STARTUPINFO si;
 
-    memset(&pi, 0, sizeof(pi));
-    memset(&si, 0, sizeof(si));
-    si.cb = sizeof(si);
-    si.lpDesktop = desktop;
+			memset(&pi, 0, sizeof(pi));
+			memset(&si, 0, sizeof(si));
+			si.cb = sizeof(si);
+			si.lpDesktop = desktop;
 
-	wchar_t cmdline[MAX_PATH];
-	wsprintf(cmdline, L"%s %s", L"NAMCredentialWizard.exe", L"/u");
-	if (CreateProcess(L"NAMCredentialWizard.exe", cmdline,
-		NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+			if (CreateProcess(NULL, lpCmdLine,
+				NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+				WaitForSingleObject(pi.hProcess, INFINITE);
+			}
+			else {
+				wchar_t msg[1000];
+				wsprintf(msg, L"CreateProcess failed (%d)", GetLastError());
+				MessageBox(NULL, msg, NULL, MB_OK);
+				break;
+			}
 
-      WaitForSingleObject(pi.hProcess, INFINITE);
-    }
-    else {
-      wchar_t msg[1000];
-      wsprintf(msg,L"CreateProcess failed (%d)",GetLastError());
-      MessageBox(NULL,msg,NULL,MB_OK);
-    }
+			if((GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0) {
+				break;
+            }
+
+			DWORD dwExitCode;
+			GetExitCodeProcess(pi.hProcess, &dwExitCode);
+			if (dwExitCode == 0) {
+				break;
+			}
+		 }
   
+        SwitchDesktop(hInputDT);
+        SetThreadDesktop(hThreadDT);
+        CloseDesktop(hNewDT);
 
-  SwitchDesktop(hInputDT);
-  SetThreadDesktop(hThreadDT);
-  CloseDesktop(hNewDT);
-
-  return 0;
-
-	} catch(...)
-	{ MessageBox(NULL,L"Caught Exception",NULL,MB_OK);
-
-
+        return 0;
+	} 
+	catch(...)
+	{ 
+		MessageBox(NULL,L"Caught Exception",NULL,MB_OK);
+		return 0;
 	}
 }
 
